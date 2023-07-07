3d1cd854d5dc68aa4a458414eac7fbfdb0327a96
Samuel Ibrahim <samuelibrahim3029@gmail.com>
2023-07-07 09:45:01 +0100

    Added json web token for auth

--- NodeProject3/Controllers/main.js
+++ NodeProject3/Controllers/main.js
@@ -1,4 +1,15 @@
+const CustomApiError = require("../Errors/customError");
+const jwt = require("jsonwebtoken");
+
 const login = async (req, res) => {
+  const { username, password } = req.body;
+
+  if (!username || !password) {
+    throw new CustomApiError("please provide email and password", 400);
+  }
+  //this is just for demo. Dababase will provide id
+  const id = new Date().getDate();
+  const token = jwt.sign({ id, username });
   res.send("Fake Login/Register/Signup Route");
 };
 
