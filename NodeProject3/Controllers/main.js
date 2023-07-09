4ce4b7de4ecd76015a9d468c4760e109b975056c
Samuel Ibrahim <samuelibrahim3029@gmail.com>
2023-07-09 10:47:29 +0100

    added javascript logic

--- NodeProject3/Controllers/main.js
+++ NodeProject3/Controllers/main.js
@@ -9,14 +9,16 @@ const login = async (req, res) => {
   }
   //this is just for demo. Dababase will provide id
   const id = new Date().getDate();
-  const token = jwt.sign({ id, username });
-  res.send("Fake Login/Register/Signup Route");
+  const token = jwt.sign({ id, username }, process.env.JWT_SECRET, {
+    expiresIn: "30d",
+  });
+  res.status(200).json({ msg: "user created", token });
 };
 
 const dashboard = async (req, res) => {
   const luckyNumber = Math.floor(Math.random() * 100);
   res.status(200).json({
-    msg: `Hello, John Doe`,
+    msg: `Hello, ${req.user.username}`,
     secret: `Here is your authorized data, your lucky number is ${luckyNumber}`,
   });
 };
