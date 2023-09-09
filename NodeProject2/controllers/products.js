fa4121588b8e674998b1dbed289a061cfe2bc4b4
Samuel Ibrahim <samuelibrahim3029@gmail.com>
2023-09-09 14:20:18 +0100

    ignored a file

--- NodeProject2/controllers/products.js
+++ NodeProject2/controllers/products.js
@@ -45,13 +45,13 @@ const getAllProducts = async (req, res) => {
 
   let result = Product.find(queryObject);
   if (sort) {
-    const sortList = sort.split(",").join("");
+    const sortList = sort.split(",").join(" ");
     const result = result.sort(sortList);
   } else {
     result = result.sort("createAt");
   }
   if (fields) {
-    const fieldList = fields.split(",").join("");
+    const fieldList = fields.split(",").join(" ");
     result = result.select(fieldList);
   }
   const page = Number(req.query.page) || 1;
